c0b1aeaf6159d7a40c99697bb6cdf67e565374b4
Almaz <aksakova53@gmail.com>
2019-11-16 13:12:46 +0300

    Initial Commit

---
+++ Learn words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn words
+//
+//  Created by MacBook on 16.11.2019.
+//  Copyright © 2019 MacPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
